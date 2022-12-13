b5d801db64859e7c4bfdca51711163445695b494
the80hz <dnl.vlgv@mail.ru>
2022-12-14 00:47:35 +0400

    working

--- main.c
+++ main.c
@@ -7,41 +7,86 @@
 #include <stdio.h>
 
 int main(){
-    printf("Введите размеры объема: ");
+    printf("(width, height, depth): ");
+    int width, height, depth;
+    scanf("%d %d %d", &width, &height, &depth);
+
+    printf("(x, y, z): ");
     int x, y, z;
     scanf("%d %d %d", &x, &y, &z);
 
-    printf("Введите координаты начальной точки: ");
-    int x0, y0, z0;
-    scanf("%d %d %d", &x0, &y0, &z0);
-
-    printf("Введите направление вектора скорости: ");
+    printf("(vx, vy, vz): ");
     int vx, vy, vz;
     scanf("%d %d %d", &vx, &vy, &vz);
 
-    printf("Введите интервал времени: ");
+    printf("(t): ");
     int t;
     scanf("%d", &t);
 
+    int bounceX = 0, bounceY = 0, bounceZ = 0;
+
     printf("Траектория движения шарика в течение %d секунд:\n", t);
-    printf("t\tx\ty\tz \n");
+    printf("t\twidth\theight\tdepth \n");
     for (int i = 0; i <= t; i++){
-        int x1 = x0 + vx * i;
-        int y1 = y0 + vy * i;
-        int z1 = z0 + vz * i;
-        if (x1 > x){
-            x1 = x - (x1 - x);
-            vx = -vx;
+        printf("%d\t%d\t%d\t%d\n", i, x, y, z);
+        printf("\n");
+        // x coordinate
+        if (bounceX == 0){
+            if(x + vx <= width){
+                x = x + vx;
+            }
+            else {
+                bounceX = 1;
+                x = width - (x + vx - width);
+            }
         }
-        if (y1 > y){
-            y1 = y - (y1 - y);
-            vy = -vy;
+        else {
+            if(x - vx >= 0){
+                x = x - vx;
+            }
+            else {
+                x = -x + vx;
+                bounceX = 0;
+            }
         }
-        if (z1 > z){
-            z1 = z - (z1 - z);
-            vz = -vz;
+        // y coordinate
+        if (bounceY == 0){
+            if(y + vy <= height){
+                y = y + vy;
+            }
+            else {
+                bounceY = 1;
+                y = height - (y + vy - height);
+            }
         }
-        printf("%d\t%d\t%d\t%d\n", i, x1, y1, z1);
+        else {
+            if(y - vy >= 0){
+                y = y - vy;
+            }
+            else {
+                y = -y + vy;
+                bounceY = 0;
+            }
+        }
+        // z coordinate
+        if (bounceZ == 0){
+            if(z + vz <= depth){
+                z = z + vz;
+            }
+            else {
+                bounceZ = 1;
+                z = depth - (z + vz - depth);
+            }
+        }
+        else {
+            if(z - vz >= 0){
+                z = z - vz;
+            }
+            else {
+                z = -z + vz;
+                bounceZ = 0;
+            }
+        }
     }
     return 0;
 }
